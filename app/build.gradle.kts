61ddb76a9c43e23aecf0a54b934d6ab58b47edd0
Vladyslav Soldatenko <teenwolf23299@gmail.com>
2026-06-22 21:59:43 +0300

    ver bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.procrastilearn.app"
     minSdk = 26
     targetSdk = 36
-    versionCode = 2
-    versionName = "1.0.0"
+    versionCode = 3
+    versionName = "1.1.0"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
