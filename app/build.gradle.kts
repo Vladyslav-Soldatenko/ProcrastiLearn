5e3dbb6df33506c7efadf0fd63d4e88347e5e6c6
Vladyslav Soldatenko <teenwolf23299@gmail.com>
2026-07-09 23:48:27 +0300

    return version number to its place

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.procrastilearn.app"
     minSdk = 26
     targetSdk = 36
-    versionCode = 3
-    versionName = "1.1.0"
+    versionCode = 7
+    versionName = "1.2.2"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
