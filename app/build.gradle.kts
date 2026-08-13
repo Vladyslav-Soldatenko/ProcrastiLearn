70c71880751658b093216742fa8948e466a471a7
Vladyslav Soldatenko <teenwolf23299@gmail.com>
2026-08-14 00:33:21 +0300

    chore: bump version to 1.3.5 and update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
     minSdk = 30
     //noinspection OldTargetApi
     targetSdk = 36
-    versionCode = 12
-    versionName = "1.3.4"
+    versionCode = 13
+    versionName = "1.3.5"
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
